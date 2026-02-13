170e13e4851bca136ebaaf00addc5a19a5ee5e24
Gustavo Yuri Nishi <gustavoyurinishi@gmail.com>
2026-02-13 14:33:32 -0300

    update

--- NetBeansProjects/Aula02Exe01/src/main/java/com/mycompany/aula02exe01/Pessoa.java
+++ NetBeansProjects/Aula02Exe01/src/main/java/com/mycompany/aula02exe01/Pessoa.java
@@ -1,13 +1,27 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.mycompany.aula02exe01;
 
-/**
- *
- * @author unifgnishi
- */
 public class Pessoa {
+    private String nome;
+    private String cpf;
+    private int idade;
     
+    void setNome(String nome){
+        this.nome = nome;
+    }
+    void setCpf(String cpf){
+        this.cpf = cpf;
+    }
+    void setIdade(int idade){
+        this.idade = idade;
+    }
+    
+    public String getNome(){
+        return this.nome;
+    }
+    public String getCpf(){
+        return this.cpf;
+    }
+    public int getIdade(){
+        return this.idade;
+    }
 }
--- NetBeansProjects/Aula02Exe01/src/main/java/com/mycompany/aula02exe01/TestePessoa.java
+++ NetBeansProjects/Aula02Exe01/src/main/java/com/mycompany/aula02exe01/TestePessoa.java
@@ -11,27 +11,71 @@ public class TestePessoa {
         int idadeLocal;
         
         Pessoa p1 = new Pessoa();
+        Pessoa p2 = new Pessoa();
+        Pessoa p3 = new Pessoa();
         
+        System.out.println("Pessoa 1");
         System.out.println("Digite o nome da 1 pessoa: ");
         nomeLocal = teclado.nextLine();
         p1.setNome(nomeLocal);
-        
         System.out.println("Digite o cpf da 1 pessoa: ");
         cpfLocal = teclado.nextLine();
         p1.setCpf(cpfLocal);
-        
         System.out.println("Digite a idade da 1 pessoa: ");
         idadeLocal = teclado.nextInt();
         p1.setIdade(idadeLocal);
         
-        System.out.println("\n");
+        teclado.nextLine();
         
+        System.out.println("Pessoa 2");
+        System.out.println("Digite o nome da 2 pessoa: ");
+        nomeLocal = teclado.nextLine();
+        p2.setNome(nomeLocal);
+        System.out.println("Digite o cpf da 2 pessoa: ");
+        cpfLocal = teclado.nextLine();
+        p2.setCpf(cpfLocal);
+        System.out.println("Digite a idade da 2 pessoa: ");
+        idadeLocal = teclado.nextInt();
+        p2.setIdade(idadeLocal);
+        
+        teclado.nextLine();
+        
+        System.out.println("Pessoa 3");
+        System.out.println("Digite o nome da 3 pessoa: ");
+        nomeLocal = teclado.nextLine();
+        p3.setNome(nomeLocal);
+        System.out.println("Digite o cpf da 3 pessoa: ");
+        cpfLocal = teclado.nextLine();
+        p3.setCpf(cpfLocal);
+        System.out.println("Digite a idade da 3 pessoa: ");
+        idadeLocal = teclado.nextInt();
+        p3.setIdade(idadeLocal);
+        
+        System.out.println("\n");
         System.out.println("--------------------------------------------");
         System.out.println("-------------Dados da 1 pessoa--------------");
         System.out.println("--------------------------------------------");
         System.out.println("Nome:......: " + p1.getNome());
         System.out.println("CPF:.......: " + p1.getCpf());
         System.out.println("Idade:.....: " + p1.getIdade());
+        System.out.println("--------------------------------------------");
+        System.out.println("\n");
+        
+        System.out.println("--------------------------------------------");
+        System.out.println("-------------Dados da 2 pessoa--------------");
+        System.out.println("--------------------------------------------");
+        System.out.println("Nome:......: " + p2.getNome());
+        System.out.println("CPF:.......: " + p2.getCpf());
+        System.out.println("Idade:.....: " + p2.getIdade());
+        System.out.println("--------------------------------------------");
+        
+        System.out.println("\n");
+        System.out.println("--------------------------------------------");
+        System.out.println("-------------Dados da 3 pessoa--------------");
+        System.out.println("--------------------------------------------");
+        System.out.println("Nome:......: " + p3.getNome());
+        System.out.println("CPF:.......: " + p3.getCpf());
+        System.out.println("Idade:.....: " + p3.getIdade());
         System.out.println("--------------------------------------------");
     }
 }
