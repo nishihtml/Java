7e01153ef07993aac2221b9df3b47560494e5718
Gustavo Yuri Nishi <gustavoyurinishi@gmail.com>
2026-03-16 15:26:59 -0300

    update

--- NetBeansProjects/Aula06Exe03/src/main/java/com/mycompany/aula06exe03/Retangulo.java
+++ NetBeansProjects/Aula06Exe03/src/main/java/com/mycompany/aula06exe03/Retangulo.java
@@ -1,13 +1,39 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.mycompany.aula06exe03;
 
-/**
- *
- * @author unifgnishi
- */
-public class Retangulo {
+public class Retangulo extends Forma{
+    private double comprimento, largura;
+
+    public Retangulo() {
+    }
+
+    public Retangulo(double comprimento, double largura, String tipo) {
+        super(tipo);
+        this.comprimento = comprimento;
+        this.largura = largura;
+    }
+
+    public double getComprimento() {
+        return comprimento;
+    }
+    public void setComprimento(double comprimento) {
+        this.comprimento = comprimento;
+    }
+    public double getLargura() {
+        return largura;
+    }
+    public void setLargura(double largura) {
+        this.largura = largura;
+    }
     
+    @Override
+    public double perimetro(){
+        return 2 * comprimento + 2 * largura;
+    }
+    @Override
+    public void print(){
+        System.out.println("Tipo.........:" + super.getTipo());
+        System.out.println("Comprimento..:" + this.comprimento);
+        System.out.println("Largura......:" + this.largura);
+        System.out.println("Perimetro....:" + this.perimetro());
+    }
 }
